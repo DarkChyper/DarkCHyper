0520bcc6aa3f9bab6d5e12059fdf1e34f6b0ae8c
Lhoir Simon <DarkChyper@users.noreply.github.com>
2022-05-23 12:51:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,15 +17,49 @@ let DATA = {
     timeZoneName: 'short',
     timeZone: 'Europe/Paris',
   }),
-};/**
-  * A - We open 'main.mustache'
-  * B - We ask Mustache to render our file with the data
-  * C - We create a README.md file with the generated output
-  */
-function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+};
+
+async function setWeatherInformation() {
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=50.4198&lon=2.7807&appId=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
+  )
+    .then(r => r.json())
+    .then(r => {
+      DATA.city_temperature = Math.round(r.main.temp);
+      DATA.city_weather = r.weather[0].description;
+      DATA.city_weather_icon = r.weather[0].icon;
+      DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'Europe/Paris',
+      });
+      DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'Europe/Paris',
+      });
+    });
+}
+
+async function generateReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
     fs.writeFileSync('README.md', output);
   });
-}generateReadMe();
+}
+
+async function action() {
+  /**
+   * Fetch Weather
+   */
+  await setWeatherInformation();
+
+  /**
+   * Generate README
+   */
+  await generateReadMe();
+
+}
+
+action();
